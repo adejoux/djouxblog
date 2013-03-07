f2e83846243f50b0bf1c1708d9020e0dade99dd2
Alain Dejoux <adejoux@krystalia.net>
2013-03-07 18:26:34 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Djouxblog::Application.config.secret_token = 'ddceb79e0cc896dc1beb9d2ad2d2a9f7ae040ab403c6fc2dc29430b2392ca05ac8c32225087d4d2462ddafbe12251a11253165af98822e4768dff4af77d067fb'
