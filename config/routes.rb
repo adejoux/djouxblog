f8253ee3b5cf3d45ea7b9d49e6fd3e5671004476
Alain Dejoux <adejoux@krystalia.net>
2013-03-07 19:15:21 +0100

    added mercury editor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Djouxblog::Application.routes.draw do
+    namespace :mercury do
+      resources :images
+    end
+
+  mount Mercury::Engine => '/'
+
   authenticated :user do
     root :to => 'home#index'
   end
