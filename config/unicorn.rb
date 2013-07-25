50aa34805584531873bb67a5ff2b734e5aa535b3
Alain Dejoux <adejoux@krystalia.net>
2013-07-25 14:15:17 +0200

    unicorn and nginx config files

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+working_directory "/home/rails/djouxblog"
+pid "/home/rails/djouxblog/tmp/pids/unicorn.pid"
+stderr_path "/home/rails/djouxblog/unicorn/err.log"
+stdout_path "/home/rails/djouxblog/unicorn/out.log"
+ 
+listen "/tmp/unicorn_djouxblog.socket"
+ 
+worker_processes 2
+timeout 30
